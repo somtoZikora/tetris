aec436e343e156cab65db353b968c96fe6e7bbee
Somto Zikora <zikorasomto@gmail.com>
2019-01-11 23:28:39 +0100

    implement boundary handlers

--- js/controls.js
+++ js/controls.js
@@ -15,13 +15,91 @@ document.addEventListener('keydown', handleKeyDownPress);
 function handleKeyDownPress(event) {
   const code = event.keyCode;
 
-  if (code === keys.FORWARD) piece.position.z -= 10;
-  if (code === keys.BACKWARD) piece.position.z += 10;
-  if (code === keys.LEFT) piece.position.x -= 10;
-  if (code === keys.RIGHT) piece.position.x += 10;
-  if (code === keys.UP) piece.position.y += 10;
-  if (code === keys.DOWN) piece.position.y -= 10;
-  if (code === keys.ROTATEX) piece.rotateZ(-Math.PI / 2);
-  if (code === keys.ROTATEY) piece.rotateX(-Math.PI / 2);
-  if (code === keys.ROTATEZ) piece.rotateY(-Math.PI / 2);
+  if (code === keys.FORWARD) forward();
+  if (code === keys.BACKWARD) backward();
+  if (code === keys.LEFT) left();
+  if (code === keys.RIGHT) right();
+  if (code === keys.UP) up();
+  if (code === keys.DOWN) down();
+  if (code === keys.ROTATEX) xRotate();
+  if (code === keys.ROTATEY) yRotate();
+  if (code === keys.ROTATEZ) zRotate();
+
+  pieceBox.setFromObject(piece);
+}
+
+function forward() {
+  if (!pieceBox.intersectsBox(bounds.end)) piece.position.z -= 10;
+  pieceBox.setFromObject(piece);
+}
+function backward() {
+  if (!pieceBox.intersectsBox(bounds.openEnd)) piece.position.z += 10;
+  pieceBox.setFromObject(piece);
+}
+function left() {
+  console.log('foo');
+  if (!pieceBox.intersectsBox(bounds.side)) piece.position.x -= 10;
+  pieceBox.setFromObject(piece);
+}
+function right() {
+  console.log('bar');
+  if (!pieceBox.intersectsBox(bounds.openSide)) piece.position.x += 10;
+  pieceBox.setFromObject(piece);
+}
+function up() {
+  if (!pieceBox.intersectsBox(bounds.ceiling)) piece.position.y += 10;
+  pieceBox.setFromObject(piece);
+}
+function down() {
+  if (!pieceBox.intersectsBox(bounds.floor)) piece.position.y -= 10;
+  pieceBox.setFromObject(piece);
+}
+
+function xRotate() {
+  piece.rotateZ(-Math.PI / 2);
+  pieceBox.setFromObject(piece);
+  adjustForRotation();
+}
+function yRotate() {
+  piece.rotateX(-Math.PI / 2);
+  pieceBox.setFromObject(piece);
+  adjustForRotation();
+}
+function zRotate() {
+  piece.rotateY(-Math.PI / 2);
+  pieceBox.setFromObject(piece);
+  adjustForRotation();
+}
+
+function adjustForRotation() {
+  if (
+    pieceBox.intersectsBox(bounds.ceiling) ||
+    pieceBox.intersectsBox(bounds.floor) ||
+    pieceBox.intersectsBox(bounds.side) ||
+    pieceBox.intersectsBox(bounds.openSide) ||
+    pieceBox.intersectsBox(bounds.end) ||
+    pieceBox.intersectsBox(bounds.openEnd)
+  ) {
+    while (
+      pieceBox.intersectsBox(bounds.ceiling) ||
+      pieceBox.intersectsBox(bounds.floor) ||
+      pieceBox.intersectsBox(bounds.side) ||
+      pieceBox.intersectsBox(bounds.openSide) ||
+      pieceBox.intersectsBox(bounds.end) ||
+      pieceBox.intersectsBox(bounds.openEnd)
+    ) {
+      if (piece.position.x < 0) right();
+      if (piece.position.x > 0) left();
+      if (piece.position.z < 0) backward();
+      if (piece.position.z > 0) forward();
+      if (piece.position.y < 0) up();
+      if (piece.position.y > 0) down();
+    }
+    if (piece.position.x < 0) left();
+    if (piece.position.x > 0) right();
+    if (piece.position.z < 0) forward();
+    if (piece.position.z > 0) backward();
+    if (piece.position.y < 0) down();
+    if (piece.position.y > 0) up();
+  }
 }
--- js/game.js
+++ js/game.js
@@ -15,9 +15,21 @@ canvas.height = window.innerHeight;
 
 scene.add(new THREE.AxisHelper(50));
 
+let bounds = {
+  ceiling: new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()),
+  floor: new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()),
+  side: new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()),
+  openSide: new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()),
+  end: new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()),
+  openEnd: new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()),
+};
+
 let field = configureField();
 scene.add(field);
+
 let piece = getRandomPiece();
+let pieceBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+
 scene.add(piece);
 
 window.addEventListener('resize', onWindowResize, false);
@@ -55,15 +67,40 @@ function configureField() {
   floor.rotateX(-Math.PI / 2);
   floor.position.y = -120;
 
+  let ceiling = configureSurface({ width: 120, height: 120, color: 'red' });
+  ceiling.rotateX(-Math.PI / 2);
+  ceiling.position.y = 120;
+  ceiling.material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+
   let end = configureSurface({ width: 120, height: 240, color: 'green' });
   end.position.z = -60;
+
+  let openEnd = configureSurface({ width: 120, height: 240, color: 'green' });
+  openEnd.position.z = 60;
+  openEnd.material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
 
   let side = configureSurface({ width: 120, height: 240, color: 'blue' });
   side.rotateY(-Math.PI / 2);
   side.position.x = -60;
 
+  let openSide = configureSurface({ width: 120, height: 240, color: 'blue' });
+  openSide.rotateY(-Math.PI / 2);
+  openSide.position.x = 60;
+  openSide.material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
+
   let field = new THREE.Group();
+
+  bounds.ceiling.setFromObject(ceiling);
+  bounds.floor.setFromObject(floor);
+  bounds.end.setFromObject(end);
+  bounds.openEnd.setFromObject(openEnd);
+  bounds.side.setFromObject(side);
+  bounds.openSide.setFromObject(openSide);
+
   field.add(floor);
+  field.add(ceiling);
+  field.add(openEnd);
+  field.add(openSide);
   field.add(end);
   field.add(side);
 
